f9fb6e7116b580c87f83c2947348ae6586d23165
hxj0316 <305625579@qq.com>
2022-09-06 11:02:36 +0800

    second commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream.h>
+#include"data_call.h"
+using namespace std;
+int main(void)
+{
+    /*调用C接口*/
+    cout<<"start to call c function"<<endl;
+
+    cout<<"end to call c function"<<endl;
+    return 0;
+}
